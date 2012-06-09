2f7dc27112bdacddcb08c9b318ce81c276eb5fae
Ron <cips173@gmail.com>
2012-06-09 13:16:13 +0530

    add p350 in gb-release-7.2

---
+++ products/cyanogen_p350.mk
@@ -0,0 +1,40 @@
+# Include GSM stuff
+$(call inherit-product, vendor/cyanogen/products/gsm.mk)
+
+# Inherit device configuration for p350.
+$(call inherit-product, device/lge/p500/p350.mk)
+
+# Inherit some common cyanogenmod stuff.
+$(call inherit-product, vendor/cyanogen/products/common_full.mk)
+
+# Include FM-Radio stuff
+$(call inherit-product, vendor/cyanogen/products/bcm_fm_radio.mk)
+
+PRODUCT_NAME := cyanogen_p350
+PRODUCT_BRAND := lge
+PRODUCT_DEVICE := p350
+PRODUCT_MODEL := LG-P350
+PRODUCT_MANUFACTURER := LGE
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BUILD_ID=GRI40 \
+    PRIVATE_BUILD_DESC="pecan-user 2.3.7 GRI90 20111018 release-keys" \
+    BUILD_FINGERPRINT=lge/pecan/pecan:2.3.7/GRI90/20111018:user/release-keys
+
+# Extra overlay for LDPI
+PRODUCT_PACKAGE_OVERLAYS += vendor/cyanogen/overlay/ldpi
+
+#Prelink map
+PRODUCT_SPECIFIC_DEFINES += TARGET_PRELINKER_MAP=$(TOP)/vendor/cyanogen/prelink-linux-arm-msm722x.map
+
+# Add LDPI assets, in addition to MDPI
+PRODUCT_LOCALES += ldpi mdpi
+
+# Release name and versioning
+PRODUCT_RELEASE_NAME := OptimusOne
+PRODUCT_VERSION_DEVICE_SPECIFIC :=
+-include vendor/cyanogen/products/common_versions.mk
+
+
+PRODUCT_COPY_FILES +=  \
+    vendor/cyanogen/prebuilt/ldpi/media/bootanimation.zip:system/media/bootanimation.zip
